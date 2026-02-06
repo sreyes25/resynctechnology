9a75a64d4039ad16327cdf954d9894db19118f92
Sergio Reyes <82613689+sreyes25@users.noreply.github.com>
2026-02-06 03:07:17 -0500

    Project form added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/server";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
