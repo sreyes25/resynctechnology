cbf8567d7404f372e2f0a524fa9df3ce9055a2dd
Sergio Reyes <82613689+sreyes25@users.noreply.github.com>
2026-02-12 17:50:00 -0500

    seo on pages

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -5,6 +5,7 @@ const routes = [
   { path: "/about", changefreq: "monthly", priority: "0.7" },
   { path: "/start", changefreq: "weekly", priority: "0.9" },
   { path: "/services/nyc-custom-software-development", changefreq: "weekly", priority: "0.95" },
+  { path: "/services/brooklyn-software-developer", changefreq: "weekly", priority: "0.9" },
   { path: "/services/websites", changefreq: "weekly", priority: "0.9" },
   { path: "/services/digital-marketing", changefreq: "weekly", priority: "0.9" },
   { path: "/invoiceamericano", changefreq: "monthly", priority: "0.6" },
